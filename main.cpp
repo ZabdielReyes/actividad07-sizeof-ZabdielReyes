3cfd67b9992264f5790067fcf5d84b93fadc3984
ZabdielReyes <70989008+ZabdielReyes@users.noreply.github.com>
2020-10-04 16:30:57 -0500

    Sizeof(() implementado con éxito

--- main.cpp
+++ main.cpp
@@ -2,8 +2,43 @@
 
 using namespace std;
 
-int main() {
-    cout << "Hola Mundo!" << endl; // eliminar esta línea
-
+int main() 
+{
+	string cadena;
+	getline(cin, cadena);
+	
+	// utilizar tipos de datos 		`char`, `int`, `short`, `float`, `double`, `long`
+	
+	if (cadena == "int")
+	{
+		cout<<"sizeof(int): " <<sizeof(int) <<endl;
+	}
+	
+	else if(cadena == "char")
+	{
+		cout<<"sizeof(char): " <<sizeof(char) <<endl;
+	}
+	
+	else if(cadena == "short")
+	{
+		cout<<"sizeof(short): " <<sizeof(short) <<endl;
+	}
+	
+	else if(cadena == "float")
+	{
+		cout<<"sizeof(float): " <<sizeof(float) <<endl;
+	}
+	
+	else if(cadena == "double")
+	{
+		cout<<"sizeof(double): " <<sizeof(double) <<endl;
+	}
+	
+	else if(cadena == "long")
+	{
+		cout<<"sizeof(long): " <<sizeof(long) <<endl;
+	}
+	
+	
     return 0;
 }
